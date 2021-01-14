85081eeb08afa7da05e99d4077f1e72954b0e8f8
tdondich <taylor@dondich.com>
2021-01-13 16:22:42 -0800

    Update Baremetrics.php

--- src/Baremetrics.php
+++ src/Baremetrics.php
@@ -27,8 +27,8 @@ class Baremetrics
     {
         $response = $this->client->get($this->metric, [
             'query' => [
-                'start_date' => now()->subMonths($months)->setTimezone('UTC')->format('Y-m-d'),
-                'end_date'   => now()->setTimezone('UTC')->format('Y-m-d'),
+                'start_date' => now()->subMonths($months)->setTimezone('PDT')->format('Y-m-d'),
+                'end_date'   => now()->setTimezone('PDT')->format('Y-m-d'),
             ],
         ])->getBody()->getContents();
         $entries = json_decode($response)->metrics;
